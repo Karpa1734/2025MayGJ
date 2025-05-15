67de11eb08848d3d59e85c34b66ea3ca3a37f746
LAPTOP-U4H6CS0Q\keike <keikeita0731@gmail.com>
2025-05-15 17:10:03 +0900

    お米と食べもの

---
+++ Assets/Script/PlayerMover.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    [SerializeField] float speed = 10f;
+    [SerializeField] float minX = -8f;
+    [SerializeField] float maxX = 8f;
+
+    void Update()
+    {
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        Vector3 move = new Vector3(moveInput * speed * Time.deltaTime, 0, 0);
+        transform.position += move;
+
+        float clampedX = Mathf.Clamp(transform.position.x, minX, maxX);
+        transform.position = new Vector3(clampedX, transform.position.y, transform.position.z);
+    }
+
+    // 2D用の衝突検出（トリガー）
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Foods"))
+        {
+            Destroy(other.gameObject); // ぶつかった相手だけ削除
+        }
+    }
+}
