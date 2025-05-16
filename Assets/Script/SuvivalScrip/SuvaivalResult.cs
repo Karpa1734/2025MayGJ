c67008824344375cc86e8e895637f7b8c4dfc0c2
madoromi <madoromi.0501.fan@gmail.com>
2025-05-16 17:21:32 +0900

    SuvaivalResultの変更

--- Assets/Script/SuvivalScrip/SuvaivalResult.cs
+++ Assets/Script/SuvivalScrip/SuvaivalResult.cs
@@ -1,34 +1,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
 public class SuvaivalResult : MonoBehaviour
 {
-[SerializeField] private GameObject resultPanel;
-[SerializeField] private Text scoreText;
-[SerializeField] private PlayerMover playerMove;
+    [SerializeField] private GameObject resultPanel;
+    [SerializeField] private Text scoreText;
+    [SerializeField] private Text TimeText;
+    [SerializeField] private PlayerMover playerMove;
 
-private void Start()
-{
-    resultPanel.SetActive(false);
-}
-
-private void OnEnable()
-{
-    falloutCount.OnGameOver += ShowResult; // イベント購読
-}
-
-private void OnDisable()
-{
-    falloutCount.OnGameOver -= ShowResult; // イベント解除
-}
-
-public void ShowResult()
-{
-    resultPanel.SetActive(true);
-    if (playerMove != null && scoreText != null)
+    private void Start()
     {
-        scoreText.text = $"摂取カロリー：{playerMove.CurrentScore}cal";
+        resultPanel.SetActive(false);
     }
-}
+
+    private void OnEnable()
+    {
+        falloutCount.OnGameOver += ShowResult; // イベント購読
+    }
+
+    private void OnDisable()
+    {
+        falloutCount.OnGameOver -= ShowResult; // イベント解除
+    }
+
+    public void ShowResult()
+    {
+        resultPanel.SetActive(true);
+        if (playerMove != null && scoreText != null)
+        {
+            scoreText.text = $"摂取カロリー：{playerMove.CurrentScore}cal";
+            TimeText.text = $"食事時間：{CountUpTimer.countUp / 60}秒";
+        }
+    }
 }
